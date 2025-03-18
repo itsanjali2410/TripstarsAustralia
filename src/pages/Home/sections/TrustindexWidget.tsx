150f62af954da821a2ea2f5d07d2e45d75b0b64f
Anjali <itsanjali2410@gmail.com>
2025-03-18 14:24:36 +0530

    THINGS TO DO FOR DOMESTIC

--- src/pages/Home/sections/TrustindexWidget.tsx
+++ src/pages/Home/sections/TrustindexWidget.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
-
-// Declare Trustindex in the global window object
-declare global {
-  interface Window {
-    Trustindex?: {
-      load: () => void;
-    };
-  }
-}
 
 // Styled Container
 const Container = styled.div`
@@ -25,47 +17,65 @@ const Container = styled.div`
 `;
 
 const TrustindexWidget: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    // Set the page title
-    document.title = "Tripstars - Best Travel Experience";
-
-    if (!document.getElementById("trustindex-script")) {
-      const script = document.createElement("script");
-      script.src =
-      "https://cdn.trustindex.io/loader.js?660bbe64210158756b963b23487";
-      script.async = true;
-      script.defer = true;
-      script.id = "trustindex-script";
-
-      script.onload = () => {
-        console.log("Trustindex script loaded");
-        if (window.Trustindex) {
-          window.Trustindex.load();
-        }
-      };
-
-      containerRef.current?.appendChild(script);
-    } else {
-      if (window.Trustindex) {
-        window.Trustindex.load();
-      }
-    }
-  }, []);
-
   return (
-    <Container>
-      {/* Page Heading */}
-      <h3 style={{ marginTop: "6%", marginBottom: "1%", textAlign: "center" }}>Google Reviews</h3>
-      <h6 style={{ marginTop: "1%", marginBottom: "4%", textAlign: "center" }}><center>Their love keep us growing</center></h6>
+    <>
+      <Helmet>
+        {/* Primary Meta Tags */}
+        <title>Tripstars - Best Travel Experience</title>
+        <meta name="description" content="Discover amazing travel destinations with Tripstars. Book your next adventure now!" />
+        <meta name="keywords" content="travel, tours, Tripstars, vacation, holidays, flights, hotels" />
+        <meta name="author" content="Tripstars Team" />
 
-      <div ref={containerRef}>
-        <div className="trustindex-widget">
-          {/* Trustindex widget will render reviews here */}
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="https://www.tripstars.in/public/upload/tripstars/images/favicon.ico" />
+        
+        {/* Open Graph for Social Media */}
+        <meta property="og:title" content="Tripstars - Best Travel Experience" />
+        <meta property="og:description" content="Explore amazing travel destinations and book your next trip with Tripstars!" />
+        <meta property="og:image" content="https://www.tripstars.in/public/upload/tripstars/images/og-image.jpg" />
+        <meta property="og:url" content="https://www.tripstars.in/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card for Social Media */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Tripstars - Best Travel Experience" />
+        <meta name="twitter:description" content="Discover amazing travel destinations with Tripstars!" />
+        <meta name="twitter:image" content="https://www.tripstars.in/public/upload/tripstars/images/twitter-image.jpg" />
+
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="YOUR_GOOGLE_SITE_VERIFICATION_CODE" />
+        
+        {/* Structured Data for SEO */}
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Tripstars",
+              "url": "https://www.tripstars.in/",
+              "logo": "https://www.tripstars.in/public/upload/tripstars/images/logo.png",
+              "description": "Discover amazing travel destinations with Tripstars.",
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+91-1234567890",
+                "contactType": "customer service"
+              }
+            }
+          `}
+        </script>
+      </Helmet>
+
+      {/* Your Page Content */}
+      <Container>
+        <h3 style={{ marginTop: "6%", marginBottom: "1%", textAlign: "center" }}>Google Reviews</h3>
+        <h6 style={{ marginTop: "1%", marginBottom: "4%", textAlign: "center" }}><center>Their love keep us growing</center></h6>
+        <div>
+          <div className="trustindex-widget">
+            {/* Trustindex widget will render reviews here */}
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   );
 };
 
