25287d1869eb005440ec4155090bafb19b91d62c
Anjali <itsanjali2410@gmail.com>
2025-03-21 11:38:56 +0530

    About us

--- src/pages/About/Sections/TechAchievment.tsx
+++ src/pages/About/Sections/TechAchievment.tsx
@@ -19,7 +19,7 @@ function AboutHistory() {
           {/* Text Section */}
           <div className="col-md-6 col-12 mt-3 text-center text-md-start">
             <h1 className="fs-2 mb-3">
-              A Little About <span></span>
+              A Little About Us<span></span>
             </h1>
             <div className="grytxtht mb-2">
               Welcome to Tripstars! We are a leading travel website dedicated to helping you plan the perfect vacation.
@@ -36,10 +36,7 @@ function AboutHistory() {
             <div className="blutxtht mb-2">
               At Tripstars, we are committed to providing the best possible travel experience for our customers.
             </div>
-            <div className="grytxtht mb-2">
-              From the moment you start planning your trip to the moment you return home, we are here to assist you
-              and make sure everything goes smoothly.
-            </div>
+           
           </div>
         </div>
       </div>
